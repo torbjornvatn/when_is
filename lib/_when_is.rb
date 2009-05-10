a5497f52fc262e23dd9f1a5e83f6525a1b4a5287
Torbjørn Vatn (jobb) <torbjorn@vatn.org>
2009-05-10 21:31:04 +0200

    This is hard

--- lib/_when_is.rb
+++ lib/_when_is.rb
@@ -1,15 +1,18 @@
-Shoes.app(:title => "when_is") do
-  flow do
-    background lavender
-    banner 'when is'
-    @before = edit_line
-    @before.text="Before"
-    @before.width=50
-
-    @text = para ""
+Shoes.app(:title => "when_is", :resizable => false) do
+  background lavender
+  banner 'when is'
+  @days = 0
+  flow(:width => 120, :margin => [3, 30, 0, 0]) do
+    @before = edit_line :text => "days", :width => 50
     @before.change do |b|
-      @text.text = b.text
+      @days = b.text
     end
-    caption "days"
   end
+  flow(:width => 120, :margin => [3, 30, 0, 0]) do
+    @after = edit_line :text => "days", :width => 50
+    @after.change do |b|
+      @days = b.text
+    end
+  end
+  para @days
 end
