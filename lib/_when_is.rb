a1e7da3b16281c72f84bb05ad0b5cad530c3d587
Torbjørn Vatn <torbjorn@vatn.org>
2009-05-11 22:52:59 +0200

    almost there

--- lib/_when_is.rb
+++ lib/_when_is.rb
@@ -9,9 +9,14 @@ class When_is
     @offset = 0
   end
   
-  def to_s
-    display_date = Date.parse("#{@year}-#{@month}-#{@day}") 
-    (display_date - @offset).to_s
+  def after
+    date = Date.parse("#{@year}-#{@month}-#{@day}") 
+     (date - @offset).to_s
+  end
+  
+  def before
+    date = Date.parse("#{@year}-#{@month}-#{@day}") 
+     (date + @offset).to_s
   end
   
   def subtract(days)
@@ -24,41 +29,52 @@ class When_is
   
 end
 
-new_date = nil
+before_date = nil
+after_date = nil
 when_is = When_is.new
-
-Shoes.app(:title => "when_is") do
-  background lavender
-  banner 'when is'
-  
-  flow(:width => 120, :margin => [3, 30, 0, 0]) do
-    edit_line :text => "days", :width => 50 do |before|
-      when_is.add before.text.to_i
-      new_date.replace when_is
+Shoes.app :title => "when_is", :width => 800, :resizable => false do
+  stack do
+    background lavender
+    title '_when is'
+    
+    flow :margin_left => 35 do
+      
+      t = caption strong(after_date), :stroke => firebrick, :margin => [0,10,5,0]
+      caption strong("="), :stroke => firebrick, :margin => [5,10,5,0]
+      caption strong("days"), :stroke => firebrick, :margin => [5,10,5,0]
+      edit_line :width => 50, :margin => [5,7,5,0] do |before|
+        when_is.add before.text.to_i
+        before_date.replace when_is.before
+        after_date.replace when_is.afterer
+      end
+      subtitle strong("-"), :stroke => firebrick, :margin => [0,0,10,0]
+      flow :width => 220, :height => 40 do
+        border firebrick, :strokewidth => 2
+        list_box :items => (1..31).to_a, :choose => 1, :width => 50, :margin => 5 do |d|
+          when_is.day = d.text
+          before_date.replace when_is.before
+          after_date.replace when_is.after
+        end
+        list_box :items => Date::ABBR_MONTHNAMES, :choose => 'Jan', :width => 70, :margin => 5 do |m|
+          when_is.month = m.text
+          before_date.replace when_is.before
+          after_date.replace when_is.after
+        end
+        list_box :items => (2009..2050).to_a, :choose => Date.today.year, :width => 100, :margin => 5 do |y|
+          when_is.year = y.text
+          before_date.replace when_is.before
+          after_date.replace when_is.after
+        end
+      end
+      subtitle strong("+"), :stroke => firebrick, :margin => [5,0,5,0]
+      edit_line :width => 50, :margin => [5,7,5,0] do |after|
+        when_is.subtract after.text.to_i
+        before_date.replace when_is.before
+        after_date.replace when_is.after
+      end
+      caption strong("days"), :stroke => firebrick, :margin => [0,10,5,0]
+      caption strong("="), :stroke => firebrick, :margin => [0,10,5,0]
+      s = caption strong(before_date), :stroke => firebrick, :margin => [0,10,5,0]
     end
   end
-  
-  flow(:width => 120, :margin => [3, 30, 0, 0]) do
-    list_box :items => (1..31).to_a, :choose => 1, :width => 50 do |d|
-      when_is.day = d.text
-      new_date.replace when_is
-    end
-    list_box :items => Date::ABBR_MONTHNAMES, :choose => 'Jan', :width => 50 do |m|
-      when_is.month = m.text
-      new_date.replace when_is
-    end
-    list_box :items => (2009..2050).to_a, :choose => Date.today.year, :width => 50 do |y|
-      when_is.year = y.text
-      new_date.replace when_is
-    end
-  end
-  
-  flow(:width => 120, :margin => [3, 30, 0, 0]) do
-    edit_line :text => "days", :width => 50 do |after|
-      when_is.subtract after.text.to_i
-      new_date.replace when_is
-    end
-  end
-  
-  new_date = para strong(when_is)
 end
