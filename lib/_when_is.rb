23db0d929e7d39898e4cc990d200bfbdd2e8db2c
Torbjørn Vatn (hjemme) <torbjorn@vatn.org>
2009-05-13 22:18:30 +0200

    experimenting with defalut margins

--- lib/_when_is.rb
+++ lib/_when_is.rb
@@ -45,46 +45,47 @@ Shoes.app :title => "when_is", :width => 800, :height => 300, :resizable => fals
     @after.replace date
   end
   
-  stack :margin => [30,30,30,0] do
+  stack do
     title '_when is'
     
     flow :margin => 20 do
-      flow(:width => 100){@before = caption strong("?"), :stroke => firebrick, :margin => [0,10,5,0], :align => 'right'}
-      caption strong("="), :stroke => firebrick, :margin => [5,10,5,0]
-      caption strong("days"), :stroke => firebrick, :margin => [5,10,5,0]
-      edit_line :width => 50, :margin => [5,7,5,0] do |days|
+      border red
+      flow(:width => 100){@before = caption strong("?"), :stroke => firebrick, :align => 'right'}
+      caption strong("="), :stroke => firebrick
+      caption strong("days"), :stroke => firebrick
+      edit_line :width => 50 do |days|
         when_is.subtract days.text.to_i
       end
-      subtitle strong("-"), :stroke => firebrick, :margin => [0,0,8,0]
+      @dash = subtitle strong(" – "), :stroke => firebrick
+      debug @dash.rise
       flow :width => 220 do
-        #border firebrick, :strokewidth => 2
-        flow(:left => -10, :top => -11) do
+        flow do
           fill oldlace
           stroke firebrick
           strokewidth 3
           rect 10, 10, 220, 40, 10
         end
-        list_box :items => (1..31).to_a, :choose => today.day, :width => 67, :margin => [10,5,5,5] do |d|
+        list_box :items => (1..31).to_a, :choose => today.day, :width => 67 do |d|
           when_is.day = d.text
           when_is.change
         end
         month_names = Date::ABBR_MONTHNAMES
-        list_box :items => month_names, :choose => month_names[today.month], :width => 70, :margin => 5 do |m|
+        list_box :items => month_names, :choose => month_names[today.month], :width => 70do |m|
           when_is.month = m.text
           when_is.change
         end
-        list_box :items => (1970..2050).to_a, :choose => today.year, :width => 80, :margin => 5 do |y|
+        list_box :items => (1970..2050).to_a, :choose => today.year, :width => 80 do |y|
           when_is.year = y.text
           when_is.change
         end
       end
-      subtitle strong("+"), :stroke => firebrick, :margin => [5,0,5,0]
-      edit_line :width => 50, :margin => [5,7,5,0] do |days|
+      caption strong(" + "), :stroke => firebrick
+      edit_line :width => 50 do |days|
         when_is.add days.text.to_i
       end
-      caption strong("days"), :stroke => firebrick, :margin => [0,10,5,0]
-      caption strong("="), :stroke => firebrick, :margin => [0,10,5,0]
-      flow(:width => 100){@after = caption strong("?"), :stroke => firebrick, :margin => [0,10,5,0], :align => 'left'}
+      caption strong("days"), :stroke => firebrick
+      caption strong("="), :stroke => firebrick
+      flow(:width => 100){@after = caption strong("?"), :stroke => firebrick, :align => 'left'}
     end
     inscription "v#{$version}", :stroke => gray, :margin_top => 90
   end
