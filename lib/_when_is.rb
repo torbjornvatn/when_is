4d6b2667fc197cb6b13aef9a88c3c700533aa8d0
Klara Vatn <klara@vatn.org>
2009-06-03 21:09:07 +0200

    Added new color scheme. Introduced a 5 pixel margin needed for Windows

--- lib/_when_is.rb
+++ lib/_when_is.rb
@@ -28,11 +28,16 @@ class When_is
   end
 end
 
-$version = 0.3
+$version = 0.4
 Shoes.app :title => "when_is", :width => 800, :height => 300, :resizable => false do
   when_is = When_is.new self
   today = when_is.today
-  background gainsboro
+  bkcolor = rgb(255,237,191)
+  border_color = rgb(247,128,60)
+  textcolor = rgb(245,72,40)
+  windows_margin = 5
+  
+  background bkcolor
   
   def before(date)
     @before.replace date
@@ -43,42 +48,42 @@ Shoes.app :title => "when_is", :width => 800, :height => 300, :resizable => fals
   end
   
   stack do
-    title '_when is'
+    title '_when is', :stroke => rgb(46,13,35), :margin => 10
     
     flow :margin_top => 60, :margin_left => 30 do
-      flow(:width => 100){@before = caption strong("?"), :stroke => firebrick, :align => 'right'}
-      caption strong("= days "), :stroke => firebrick
-      edit_line :width => 50 do |days|
+      flow(:width => 100){@before = caption strong("?"), :stroke => textcolor, :align => 'right'}
+      caption strong("= days "), :stroke => textcolor
+      edit_line :width => 50, :margin => windows_margin do |days|
         when_is.subtract days.text.to_i
       end
-      caption strong(" –   "), :stroke => firebrick
+      caption strong(" –   "), :stroke => textcolor
       flow :width => 220 do
         flow do
-          fill gainsboro
-          stroke firebrick
+          fill bkcolor
+          stroke border_color
           strokewidth 3
           rect -5, -5, 225, 40, 10
         end
-        list_box :items => (1..31).to_a, :choose => today.day, :width => 67 do |d|
+        list_box :items => (1..31).to_a, :choose => today.day, :width => 67, :margin => windows_margin do |d|
           when_is.day = d.text
           when_is.change
         end
         month_names = Date::ABBR_MONTHNAMES
-        list_box :items => month_names, :choose => month_names[today.month], :width => 70 do |m|
+        list_box :items => month_names, :choose => month_names[today.month], :width => 70, :margin => windows_margin do |m|
           when_is.month = m.text
           when_is.change
         end
-        list_box :items => (1970..2050).to_a, :choose => today.year, :width => 80 do |y|
+        list_box :items => (1970..2050).to_a, :choose => today.year, :width => 80, :margin => windows_margin do |y|
           when_is.year = y.text
           when_is.change
         end
       end
-      caption strong(" + "), :stroke => firebrick
-      edit_line :width => 50 do |days|
+      caption strong(" + "), :stroke => textcolor
+      edit_line :width => 50, :margin => windows_margin do |days|
         when_is.add days.text.to_i
       end
-      caption strong("days ="), :stroke => firebrick
-      flow(:width => 100){@after = caption strong("?"), :stroke => firebrick, :align => 'left'}
+      caption strong("days ="), :stroke => textcolor
+      flow(:width => 100){@after = caption strong("?"), :stroke => textcolor, :align => 'left'}
     end
     inscription "v#{$version}", :stroke => gray, :margin_top => 90
   end
